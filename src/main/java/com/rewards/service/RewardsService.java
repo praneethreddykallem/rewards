fce9e4f3cb3de92eb0d66fb9f399d746b5e4e101
Praneeth <Praneeth@192.168.1.9>
2021-03-04 18:38:14 -0600

    add in memory db support

--- src/main/java/com/rewards/service/RewardsService.java
+++ src/main/java/com/rewards/service/RewardsService.java
@@ -1,86 +1,14 @@
 package com.rewards.service;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.stereotype.Service;
+import com.rewards.domain.Transaction;
 
-import com.rewards.dto.TransactionData;
+public interface RewardsService {
 
-@Service
-public class RewardsService {
+	Map<String, Map<String, Integer>> getRewardPoints();
 
-	private static final String TOTAL = "TOTAL";
-
-	public Map<String, Map<String, Integer>> calculate(List<TransactionData> transactionsData) {
-		Map<String, Map<String, Integer>> rewardsMap = new HashMap<>();
-
-		transactionsData.forEach(transaction -> {
-			if (rewardsMap.containsKey(transaction.getCustomerName())) {
-				addRewardPoints(rewardsMap, transaction);
-			} else {
-				addCustomer(rewardsMap, transaction);
-			}
-		});
-		return rewardsMap;
-	}
-
-	private void addRewardPoints(Map<String, Map<String, Integer>> rewardsMap, TransactionData transaction) {
-		Map<String, Integer> customerMap = rewardsMap.get(transaction.getCustomerName());
-		String transactionMonth = getTransactionMonth(transaction.getTransactionDate());
-
-		if (customerMap.containsKey(transactionMonth)) {
-			Integer rewards = customerMap.get(transactionMonth);
-			customerMap.put(transactionMonth, rewards + calculatedPoints(transaction.getAmount()));
-		} else {
-			customerMap.put(transactionMonth, calculatedPoints(transaction.getAmount()));
-		}
-
-		customerMap.put(TOTAL, calculateTotalPoints(customerMap));
-		rewardsMap.put(transaction.getCustomerName(), customerMap);
-	}
-
-	private void addCustomer(Map<String, Map<String, Integer>> rewardsMap, TransactionData transaction) {
-		Map<String, Integer> customerMap = new HashMap<>();
-		customerMap.put(getTransactionMonth(transaction.getTransactionDate()),
-				calculatedPoints(transaction.getAmount()));
-		customerMap.put(TOTAL, calculatedPoints(transaction.getAmount()));
-		rewardsMap.put(transaction.getCustomerName(), customerMap);
-	}
-
-	private Integer calculatedPoints(Integer amount) {
-		if (amount > 100) {
-			return 50 + (amount - 100) * 2;
-		} else if (amount > 50) {
-			return amount - 50;
-		}
-		return 0;
-	}
-
-	private String getTransactionMonth(String transactionDate) {
-		DateFormat format = new SimpleDateFormat("MM-dd-yyyy");
-		try {
-			Date date = format.parse(transactionDate);
-			LocalDate localdDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-			return localdDate.getMonth().toString();
-		} catch (ParseException e) {
-		}
-		return null;
-	}
-
-	private Integer calculateTotalPoints(Map<String, Integer> customerMap) {
-		Integer totalPoints = 0;
-		for (Map.Entry<String, Integer> entry : customerMap.entrySet())
-			if (!entry.getKey().equals(TOTAL))
-				totalPoints = totalPoints + entry.getValue();
-		return totalPoints;
-	}
+	void saveTransactions(List<Transaction> transactionsData);
 
 }
